8a69f6c28f769a05b6aebe1cf614796d3e52238d
SaikatVIAS <152478756+SaikatVIAS@users.noreply.github.com>
2023-12-05 18:44:08 +0530

    search page

--- resources/views/livewire/widgets/my-highlights.blade.php
+++ resources/views/livewire/widgets/my-highlights.blade.php
@@ -1,40 +1,40 @@
 <div class="vi-modal-inner">
-    <div class="vi-modal-content">
-        <div class="vi-note highlights">
-            <div class="vi-note-title-wrap">
-                <p class="vi-note-title">Highlight 1</p>
-                <a href="#" class="vi-icons note-delete"></a>
-            </div>
-            <div class="note-content">
-                <p class="vi-text-dark">Eget velit aliquet sagittis id. Sed vulputate odio ut enim blandit. Amet justo donec enim diam vulputate ut pharetra sit.</p>
-            </div>
-        </div>
-        <div class="vi-note highlights">
-            <div class="vi-note-title-wrap">
-                <p class="vi-note-title">Highlight 2</p>
-                <a href="#" class="vi-icons note-delete"></a>
-            </div>
-            <div class="note-content">
-                <p class="vi-text-dark">Eu scelerisque felis imperdiet proin fermentum leo vel orci porta. Aliquet risus feugiat in ante metus dictum at. Consequat nisl vel pretium lectus. Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Elementum tempus egestas sed sed risus pretium.</p>
-            </div>
-        </div>
-        <div class="vi-note highlights">
-            <div class="vi-note-title-wrap">
-                <p class="vi-note-title">Highlight 3</p>
-                <a href="#" class="vi-icons note-delete"></a>
-            </div>
-            <div class="note-content">
-                <p class="vi-text-dark">Sed libero enim sed faucibus turpis in eu.</p>
-            </div>
-        </div>
-        <div class="vi-note highlights">
-            <div class="vi-note-title-wrap">
-                <p class="vi-note-title">Highlight 4</p>
-                <a href="#" class="vi-icons note-delete"></a>
-            </div>
-            <div class="note-content">
-                <p class="vi-text-dark">usto nec ultrices dui sapien. Auctor urna nunc id cursus metus aliquam eleifend mi. At in tellus integer feugiat scelerisque varius morbi enim nunc.</p>
-            </div>
-        </div>
-    </div>
-</div>
+			<div class="vi-modal-content">
+				<div class="vi-note highlights">
+					<div class="vi-note-title-wrap">
+						<p class="vi-note-title">Highlight 1</p>
+						<a href="#" class="vi-icons note-delete"></a>
+					</div>
+					<div class="note-content">
+						<p class="vi-text-dark">Eget velit aliquet sagittis id. Sed vulputate odio ut enim blandit. Amet justo donec enim diam vulputate ut pharetra sit.</p>
+					</div>
+				</div>
+				<div class="vi-note highlights">
+					<div class="vi-note-title-wrap">
+						<p class="vi-note-title">Highlight 2</p>
+						<a href="#" class="vi-icons note-delete"></a>
+					</div>
+					<div class="note-content">
+						<p class="vi-text-dark">Eu scelerisque felis imperdiet proin fermentum leo vel orci porta. Aliquet risus feugiat in ante metus dictum at. Consequat nisl vel pretium lectus. Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Elementum tempus egestas sed sed risus pretium.</p>
+					</div>
+				</div>
+				<div class="vi-note highlights">
+					<div class="vi-note-title-wrap">
+						<p class="vi-note-title">Highlight 3</p>
+						<a href="#" class="vi-icons note-delete"></a>
+					</div>
+					<div class="note-content">
+						<p class="vi-text-dark">Sed libero enim sed faucibus turpis in eu.</p>
+					</div>
+				</div>
+				<div class="vi-note highlights">
+					<div class="vi-note-title-wrap">
+						<p class="vi-note-title">Highlight 4</p>
+						<a href="#" class="vi-icons note-delete"></a>
+					</div>
+					<div class="note-content">
+						<p class="vi-text-dark">usto nec ultrices dui sapien. Auctor urna nunc id cursus metus aliquam eleifend mi. At in tellus integer feugiat scelerisque varius morbi enim nunc.</p>
+					</div>
+				</div>
+			</div>
+		</div>
