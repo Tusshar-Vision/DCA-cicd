3e01c505de35f180ba3ecf87c073f02588604d47
SaikatVIAS <152478756+SaikatVIAS@users.noreply.github.com>
2023-12-06 15:53:00 +0530

    update popup

--- resources/views/components/widgets/highlights-sidebar.blade.php
+++ resources/views/components/widgets/highlights-sidebar.blade.php
@@ -12,11 +12,14 @@
         <div class="vi-announcement-card">
             <p class="vi-announcement-title">News Updates</p>
             <ul>
-                <li><span>Global Multidimensional Poverty Index (MPI) 2023 unveiled by United Nations Development Program (UNDP)</span>
+                <li><span class="limited-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by United Nations Development Program (UNDP)</span>
                     <div class="hidden-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit</div>
                 </li>
-                <li>Roadmap for Green Hydrogen Ecosystem in India by Ministry of New and Renewable Energy
-                <div class="hidden-text">Global Multidimensional Poverty Inde</div>
+                <li><span class="limited-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by United Nations Development Program (UNDP)</span>
+                    <div class="hidden-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit</div>
+                </li>
+                <li><span class="limited-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by United Nations Development Program (UNDP)</span>
+                    <div class="hidden-text">Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit Global Multidimensional Poverty Index (MPI) 2023 unveiled by Unit</div>
                 </li>
             </ul>
         </div>
