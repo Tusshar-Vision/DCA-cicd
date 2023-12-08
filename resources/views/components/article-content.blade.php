bb42bfeda2cd6c9134824007f81e3fc73d862610
vikram kumar <vikram.kumar@visionias.in>
2023-12-08 13:25:15 +0530

    notes feature completed

--- resources/views/components/article-content.blade.php
+++ resources/views/components/article-content.blade.php
@@ -95,7 +95,29 @@
     doc.addEventListener('mouseup', handleSelection);
     var pageX, pageY;
 
+    function getSelectionText() {
+        var selectedText = ""
+        if (window.getSelection) { // all modern browsers and IE9+
+            selectedText = window.getSelection().toString()
+        }
+        return selectedText
+    }
+
+    // document.addEventListener('mouseup', function() {
+    //     var thetext = getSelectionText()
+    //     if (thetext.length > 0) { // check there's some text selected
+    //         console.log(thetext) // logs whatever textual content the user has selected on the page
+    //     }
+    // }, false)
+
     function hidePopup() {
+        let editorContent = document.getElementById("notes-text-area").value
+        var selectedText = getSelectionText()
+        if (selectedText.length > 0) {
+            console.log(selectedText)
+            editorContent += selectedText;
+            tinymce.activeEditor.setContent(editorContent)
+        }
         document.getElementById("tooltip-box").style.display = "none"
     }
 
