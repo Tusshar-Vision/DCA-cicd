e7408dbcc169959012dd6429e8f30eddeb12ec12
Yash Tiwari <yash.tiwari@visionias.in>
2023-12-08 14:24:56 +0530

    Updated initiatives header to remove register button when user is logged in

--- resources/views/components/navigation/initiatives.blade.php
+++ resources/views/components/navigation/initiatives.blade.php
@@ -92,7 +92,9 @@
                     <input type="text" class="search-field" placeholder="Search..." />
                 </div>
             </li>
-            <li class="pl-[20px] pr-[20px]"><a href="#" class="register">Register</a></li>
+            @guest
+                <li class="pl-[20px] pr-[20px]"><a href="#" class="register">Register</a></li>
+            @endguest
             <li class="pl-[20px]" style="border-left: 1px solid #E5EAF4;">
             @guest
                 <button @click="isLoginFormOpen = !isLoginFormOpen"  class="flex items-center">
@@ -106,7 +108,7 @@
             @auth
                 <div class="flex items-center font-bold cursor-pointer user-style" x-data="{ isUserMenuOpen: false }" @click="isUserMenuOpen = true">
                     {{ Auth::user()->name ?? 'No Name' }}
-                    <span>A</span>
+                    <span>Y</span>
                     <!-- <svg class="mr-3" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M20 22H18V20C18 18.3431 16.6569 17 15 17H9C7.34315 17 6 18.3431 6 20V22H4V20C4 17.2386 6.23858 15 9 15H15C17.7614 15 20 17.2386 20 20V22ZM12 13C8.68629 13 6 10.3137 6 7C6 3.68629 8.68629 1 12 1C15.3137 1 18 3.68629 18 7C18 10.3137 15.3137 13 12 13ZM12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" fill="#040404"/>
                     </svg> -->
