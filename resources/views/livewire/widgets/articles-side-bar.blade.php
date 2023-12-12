e53b265f6a3106523ebc1048a8f828e0540e1755
SaikatVIAS <152478756+SaikatVIAS@users.noreply.github.com>
2023-12-12 11:36:52 +0530

    update html

--- resources/views/livewire/widgets/articles-side-bar.blade.php
+++ resources/views/livewire/widgets/articles-side-bar.blade.php
@@ -9,7 +9,7 @@
     <div class="my-4 mx-6" x-data="{ expanded: null }" x-init="expanded = 'topic-{{ Str::slug($currentTopic) }}'">
         <!-- @foreach ($topics as $topic) -->
             <h4 class="font-bold text-base[16px] py-[16px] border-bottom">Table of Content</h4>
-            <div class="">
+            <div>
                 <ol class="ml-[24px] list-decimal">
                     <li class="py-[15px] border-bottom hover:brand-color"><a href="#" class="block text-base[16px] font-medium black-040404 hover:brand-color">What Is Ethereum 2.0?</a></li>
                     <li class="py-[15px] border-bottom hover:brand-color"><a href="#" class="block text-base[16px] font-medium black-040404 hover:brand-color">What Is Ethereum 2.0?</a></li>
