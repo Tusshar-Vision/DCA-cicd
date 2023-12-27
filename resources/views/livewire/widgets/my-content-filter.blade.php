6879aa17fec3f2e7b1c89c8392f53b221d8eb9b9
SaikatVIAS <152478756+SaikatVIAS@users.noreply.github.com>
2023-12-27 14:14:34 +0530

    update filter style

--- resources/views/livewire/widgets/my-content-filter.blade.php
+++ resources/views/livewire/widgets/my-content-filter.blade.php
@@ -1,44 +1,36 @@
 <div>
 
     <label class="mb-[10px] text-[#5A7184] text-sm flex">Paper</label>
-    <div class="rounded-md broder-[#C3CAD9] text-[#5A7184] w-full">
-        <select id="cars">
-            <option value="volvo">GS-1</option>
-            <option value="saab">GS-2</option>
-            <option value="opel">GS-3</option>
-            <option value="audi">GS-4</option>
-        </select>
-    </div>
+    <select id="paper" class="rounded-md mb-[20px] broder-[#C3CAD9] text-[#5A7184] w-full">
+        <option value="volvo">GS-1</option>
+        <option value="saab">GS-2</option>
+        <option value="opel">GS-3</option>
+        <option value="audi">GS-4</option>
+    </select>
 
     <label class="mb-[10px] mt-[20px] text-[#5A7184] text-sm flex">Subject</label>
-    <div class="rounded-md broder-[#C3CAD9] text-[#5A7184] w-full">
-        <select id="cars">
-            <option value="volvo">Economy</option>
-            <option value="saab">History</option>
-            <option value="opel">Geography</option>
-            <option value="audi">Economy</option>
-        </select>
-    </div>
+    <select id="sub" class="rounded-md mb-[20px] broder-[#C3CAD9] text-[#5A7184] w-full">
+        <option value="volvo">Economy</option>
+        <option value="saab">History</option>
+        <option value="opel">Geography</option>
+        <option value="audi">Economy</option>
+    </select>
 
     <label class="mb-[10px] mt-[20px] text-[#5A7184] text-sm flex">Section</label>
-    <div class="rounded-md broder-[#C3CAD9] text-[#5A7184] w-full">
-        <select id="cars">
-            <option value="volvo">Government Budgeting and Financial Interme...</option>
-            <option value="saab">Government Budgeting and Financial Interme...</option>
-            <option value="opel">Government Budgeting and Financial Interme...</option>
-            <option value="audi">Government Budgeting and Financial Interme...</option>
-        </select>
-    </div>
+    <select id="section" class="rounded-md mb-[20px] broder-[#C3CAD9] text-[#5A7184] w-full">
+        <option value="volvo">Government Budgeting and Financial Interme...</option>
+        <option value="saab">Government Budgeting and Financial Interme...</option>
+        <option value="opel">Government Budgeting and Financial Interme...</option>
+        <option value="audi">Government Budgeting and Financial Interme...</option>
+    </select>
 
     <label class="mb-[10px] mt-[20px] text-[#5A7184] text-sm flex">Subsection</label>
-    <div class="rounded-md broder-[#C3CAD9] text-[#5A7184] w-full">
-        <select id="cars">
-            <option value="volvo">Employment</option>
-            <option value="saab">Employment</option>
-        </select>
-    </div>
+    <select id="subsection" class="rounded-md mb-[25px] broder-[#C3CAD9] text-[#5A7184] w-full">
+        <option value="volvo">Employment</option>
+        <option value="saab">Employment</option>
+    </select>
 
-    <div class="flex justify-end mt-[25px]">
+    <div class="flex justify-end">
         <button class="vi-outline-button h-10 mr-[15px] rounded-tr-md rounded-br-md w-32 cursor-pointer flex items-center justify-center">Cancel</button>
         <button class="vi-primary-button vi-search-btn h-10 rounded-tr-md rounded-br-md w-32 cursor-pointer flex items-center justify-center">Apply</button>
     </div>
