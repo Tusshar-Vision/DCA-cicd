fa9cbb83b6332003b0d0a5999acdf3a2426a9b27
Yash Tiwari <yash.tiwari@visionias.in>
2024-01-19 13:43:46 +0530

    Fixed problem with rejected articles not being listed

--- app/Livewire/Forms/Login.php
+++ app/Livewire/Forms/Login.php
@@ -20,37 +20,7 @@ class Login extends Component
 
     public function login()
     {
-        try {
-            $result = $this->client->initiateAuth([
-                'AuthFlow' => 'USER_PASSWORD_AUTH',
-                'ClientId' => $this->client_id,
-                'UserPoolId' => $this->user_pool_id,
-                'AuthParameters' => [
-                    'USERNAME' => $this->email,
-                    'PASSWORD' => $this->password,
-                ],
-            ]);
 
-            // Authentication successful
-            $accessToken = $result['AuthenticationResult']['AccessToken'];
-            $idToken = $result['AuthenticationResult']['IdToken'];
-            $refreshToken = $result['AuthenticationResult']['RefreshToken'];
-
-            $user = Student::where('email', $this->email)->first();
-            if ($user) auth('cognito')->login($user);
-
-            // You can now use the tokens as needed
-            // echo 'Access Token: ' . $accessToken . PHP_EOL;
-            // echo 'Id Token: ' . $idToken . PHP_EOL;
-            // echo 'Refresh Token: ' . $refreshToken . PHP_EOL;
-            return redirect()->route('home');
-        } catch (AwsException $e) {
-            // Authentication failed
-            Log::info('Error: ' . $e->getAwsErrorMessage());
-        } catch (\Exception $e) {
-            // Catch any other exception
-            Log::info('Unhandled Exception: ' . $e->getMessage());
-        }
     }
 
     public function render()
