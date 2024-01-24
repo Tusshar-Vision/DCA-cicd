2cbf433f303e1bdfb0cb03378f14270140f3cb5a
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-01-24 18:01:05 +0530

    Update student my content section

--- resources/views/livewire/widgets/student-bookmark-section.blade.php
+++ resources/views/livewire/widgets/student-bookmark-section.blade.php
@@ -1,26 +1,28 @@
 <!-- bookmark content -->
-<div class="vi-profile-tab-container">
-    <div class="my-content-tab-wrapper">
-        <div class="my-contnet-tab-filter">
-            <div class="my-content-search">
-                <div class="search-bar-wrapper">
-                    <input type="search" class="vi-search-bar" placeholder="Search by article name" required="">
-                    <span class="vi-icons search"></span>
+<div class="vigrid-wide">
+    <div class="vi-profile-tab-container">
+        <div class="my-content-tab-wrapper">
+            <div class="my-contnet-tab-filter">
+                <div class="my-content-search">
+                    <div class="search-bar-wrapper">
+                        <input type="search" class="vi-search-bar" placeholder="Search by article name" required="">
+                        <span class="vi-icons search"></span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="bookmark-list-wrap">
-            <ul>
-                @foreach ($bookmarks as $bookmark)
-                    <li>
-                        <img src="{{ URL::asset('images/card-image-small.png') }}">
-                        <div class="bookmark-cont">
-                            <span>{{ $bookmark->published_at }}</span>
-                            <p>{{ $bookmark->title }}</p>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+            <div class="bookmark-list-wrap">
+                <ul>
+                    @foreach ($bookmarks as $bookmark)
+                        <li>
+                            <img src="{{ URL::asset('images/card-image-small.png') }}">
+                            <div class="bookmark-cont">
+                                <span>{{ $bookmark->published_at }}</span>
+                                <p>{{ $bookmark->title }}</p>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/livewire/widgets/student-profile-tabsection.blade.php
+++ resources/views/livewire/widgets/student-profile-tabsection.blade.php
@@ -1,7 +1,7 @@
   <?php $segment = request()->segment(count(request()->segments())); ?>
   <section class="vi-user-profile-section">
       <div class="vigrid-wide">
-          <div class="vi-user-profile-tab-wrapper">
+          <div class="vi-user-profile-tab-wrapper flex-none justify-start lg:flex lg:justify-between">
               <div class="vi-profile-tab-list">
                   <a href="{{ route('user.dashboard') }}"
                       class="tab-item tab-activity {{ $segment == 'activity' ? 'active' : '' }}">Activity</a>
@@ -11,7 +11,14 @@
                       class="tab-item tab-my-content {{ $segment == 'content' ? 'active' : '' }}"">My Content</a>
                   <div class="tab-item tab-leaderboard">Leaderboard</div>
               </div>
-
+              <!-- right side tab link newly added -->
+              <ul class="flex justify-start mt-[30px] xl:mt-0 xl:justify-between text-[16px] text-[#8F93A3]">
+                <li><a href="javascript:void(0)" class="hover:text-[#3362CC] activeSubTab">Note</a></li>
+                <li><a href="javascript:void(0)" class="ml-5 hover:text-[#3362CC]">Draft</a></li>
+                <li><a href="javascript:void(0)" class="ml-5 hover:text-[#3362CC]">Delete</a></li>
+              </ul>
+              <!-- right side tab link newly added -->
           </div>
+          
       </div>
   </section>
