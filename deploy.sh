91665b0f7fc25821a97107f585383ffdd769c510
Yash Tiwari <yash.tiwari@visionias.in>
2024-01-24 14:11:29 +0530

    Calendar fix

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/github
 # bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH=$BUN_INSTALL/bin:$PATH
-# Node Setup
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# Navigate to the Laravel project directory
-cd /var/www/vision-ca-api/
+#export BUN_INSTALL="$HOME/.bun"
+#export PATH=$BUN_INSTALL/bin:$PATH
+## Node Setup
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+## Navigate to the Laravel project directory
+#cd /var/www/vision-ca-api/
 # Pull the latest changes from the GitHub repository
-git pull origin staging
+git pull origin main
 # Install/update dependencies
 composer install --no-interaction --prefer-dist --optimize-autoloader
 bun install
@@ -20,3 +20,4 @@ bun run build
 php artisan migrate --force
 # Clear caches
 php artisan optimize:clear
+php artisan optimize
--- resources/views/livewire/widgets/news-today-calendar.blade.php
+++ resources/views/livewire/widgets/news-today-calendar.blade.php
@@ -1,5 +1,5 @@
 <div class="calendar-wrapper border-1 border-color-C3CAD9 bg-white border rounded relative">
-    <div class="calender-wrap absolute left-0 top-0 mt-[23px] w-full bar hidden calShow">
+    <div class="calender-wrap absolute left-0 top-0 mt-[23px] w-full bar hidden calendar">
         <div class="vi-daily-news-card">
             <div class="flex justify-between items-center">
                 <label>
@@ -73,5 +73,19 @@
         <path d="M9.74805 4.7998V7.7998" stroke="#8F93A3" stroke-linecap="round"/>
         <path d="M17.687 4.7998V7.7998" stroke="#8F93A3" stroke-linecap="round"/>
     </svg>
-    <input type="text" class="border-0 focus:outline-none focus:border-0 w-full" id="showCal"/>
+    <input type="text" class="border-0 focus:outline-none focus:border-0 w-full" id="showCalendar"/>
 </div>
+@script
+    <script>
+        let input = document.getElementById('showCalendar');
+        let calendar = document.getElementsByClassName('calendar')[0];
+
+        input.addEventListener('focus', function() {
+            calendar.style.display = 'block';
+        });
+
+        input.addEventListener('focusout', function () {
+            calendar.style.display = 'none';
+        });
+    </script>
+@endscript
