3c106b08ad3afbb8c05d7fb49402a4322c5ff7f9
Yash Tiwari <yash.tiwari@visionias.in>
2024-01-31 11:34:41 +0530

    Bug fixes and code optimization in multiple pages

--- app/Livewire/Widgets/SearchBox.php
+++ app/Livewire/Widgets/SearchBox.php
@@ -2,19 +2,26 @@
 
 namespace App\Livewire\Widgets;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class SearchBox extends Component
 {
     public $searchTerm = '';
 
-    public function mount()
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function mount(): void
     {
         $this->searchTerm = request()->get('query');
     }
 
-    public function search() {
-
+    public function search()
+    {
         $this->validate([
             'searchTerm' => 'required|min:2', // Add validation rules as needed
         ]);
@@ -22,7 +29,7 @@ class SearchBox extends Component
         return $this->redirect('/search?query=' . $this->searchTerm, navigate: true);
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.widgets.search-box');
     }
--- resources/views/livewire/widgets/search-box.blade.php
+++ resources/views/livewire/widgets/search-box.blade.php
@@ -1,9 +1,11 @@
 <div>
-    <input
-        type="text"
-        placeholder="Search"
-        wire:model="searchTerm"
-        wire:keydown.enter="search"
-        class="w-full py-1 text-gray-700 border-none rounded-l-full focus:outline-none focus:ring-0"
-    >
+    <label>
+        <input
+            type="search"
+            placeholder="Search"
+            wire:model="searchTerm"
+            wire:keydown.enter="search"
+            class="search-field"
+        >
+    </label>
 </div>
