add006d604e945df63886ac2116eff650f735ba0
Vikram <itsworkingmind@gmail.com>
2024-02-01 14:52:32 +0530

    minimize button in archive page fixed

--- resources/views/livewire/widgets/archives/monthly-magazine.blade.php
+++ resources/views/livewire/widgets/archives/monthly-magazine.blade.php
@@ -32,13 +32,17 @@
             <h4 class="text-[#040404] text-[32px] font-normal">{{$year}}</h4>
         </div>
         <div>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 11V13H19V11H5Z" fill="#8F93A3"/>
-            </svg>
-            <svg class="hidden" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="#8F93A3"/>
-            </svg>
-        </div>
+                    <div x-show="expanded === true">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5 11V13H19V11H5Z" fill="#8F93A3"/>
+                        </svg>
+                    </div>
+                    <div x-show="expanded === false">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="#8F93A3"/>
+                        </svg>
+                    </div>
+                </div>
     </div>
 
     @foreach ($article as $a) 
