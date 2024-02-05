69f838e8ebb2c84461106cc4cab7b901fecae8a5
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-02-05 19:20:33 +0530

    Updated footer links

--- resources/views/components/footer/index.blade.php
+++ resources/views/components/footer/index.blade.php
@@ -13,7 +13,7 @@
                 </a>
             </div>
 
-            <div class="flex space-x-3 justify-center md:justify-start">
+            {{-- <div class="flex space-x-3 justify-center md:justify-start">
                 <a href="{{ UrlHelper::linkToSocial('whatsapp') }}" target="_blank">
                     <span>
                         {!! SvgIconsHelper::getSvgIcon('whatsapp-footer') !!}
@@ -49,22 +49,24 @@
                         {!! SvgIconsHelper::getSvgIcon('instagram-footer') !!}
                     </span>
                 </a>
-            </div>
-            <div class="flex flex-col md:flex-row text-center md:text-justify space-x-5 text-sm pb-4">
+            </div> --}}
+
+            <div class="flex flex-col text-center md:text-justify text-sm pb-4">
                 <span>
                     <h5 class="text-gray-400">{{ __('footer.call_us') }}</h5>
                     <p class="text-sm">
-                        <a href="tel:+91 846 802 2022" class="hover:text-[#005FAF] lg:inline-block">+91 846 802 2022,</a>
-                        <a href="tel:+91 901 906 6066" class="hover:text-[#005FAF] lg:inline-block">+91 901 906 6066</a>
+                        {{-- <a href="tel:+91 846 802 2022" class="hover:text-[#005FAF] lg:inline-block">+91 846 802 2022,</a>
+                        <a href="tel:+91 901 906 6066" class="hover:text-[#005FAF] lg:inline-block">+91 901 906 6066</a> --}}
+                        <a href="tel:+91 901 906 6066" class="hover:text-[#005FAF] lg:inline-block">+91 8468022022</a></p>
                     </p>
                 </span>
-                <span class="hidden md:block">
+                {{-- <span class="hidden md:block">
                     <svg xmlns="http://www.w3.org/2000/svg" width="2" height="38" viewBox="0 0 2 38" fill="none">
                         <path d="M1 0V38" stroke="#8F93A3"/>
                     </svg>
-                </span>
-                <span class="mt-[10px] md:mt-0">
-                    <h5 class="text-gray-400">{{ __('footer.email_us') }}</h5>
+                </span> --}}
+                <span>
+                    <h5 class="text-gray-400 mt-2">{{ __('footer.email_us') }}</h5>
                     <p class="text-sm">
                         <a href="mailto:enquiry@visionias.in" class="hover:text-[#005FAF]">enquiry@visionias.in</a>
                     </p>
@@ -73,24 +75,79 @@
             </div>
         </div>
         <div class="flex flex-col md:flex-row space-x-0 md:space-x-28 mt-[25px] md:mt-0">
-            <div class="space-y-2 border-t-[1px] md:border-t-0 py-[15px] md:py-0">
-                <h5 class="font-bold hidden md:block">{{__('footer.policy')}}</h5>
-                <h5 class="font-bold flex justify-between md:hidden" onclick="toggleList(this)">{{__('footer.policy')}} <span class="plus">+</span></h5>
+            <div class="space-y-2 border-t-[1px] border-b-[1px] md:border-t-0 md:border-b-0 py-[15px] md:py-0">
+                <h5 class="font-bold hidden md:block uppercase">COURSES</h5>
+                <h5 class="font-bold flex justify-between md:hidden uppercase" onclick="toggleList(this)">COURSES<span class="plus block md:hidden">+</span></h5>
 
                 <ul class="space-y-2 hidden md:block">
-                    <li><a href="{{ UrlHelper::linkToVision('/privacy') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.privacy_policy')}}</a></li>
-                    <li><a href="{{ UrlHelper::linkToVision('/refund-policy') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.refund_policy')}}</a></li>
-                    <li><a href="{{ UrlHelper::linkToVision('/terms-of-use') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.terms_of_use')}}</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Home</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Classroom</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Mains Test Series</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Prelims Test Series</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Interview</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Lakshya</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Daksha</a></li>
+
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Essay</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">CSAT</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">GS Mains Advance</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Fast Track - Prelims</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Hindi FC</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">PT 365</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Mains 365</a></li>
+                    <li class="block md:hidden"><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Monthly CA</a></li>
+                </ul>
+            </div>
+            
+            <div class="space-y-2 border-t-[1px] border-b-[1px] md:border-t-0 md:border-b-0 py-[15px] md:py-0 hidden md:block">
+                <ul class="space-y-2 hidden md:block mt-8">
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Essay</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">CSAT</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">GS Mains Advance</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Fast Track - Prelims</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Hindi FC</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">PT 365</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Mains 365</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Monthly CA</a></li>
+                </ul>
+            </div>
+
+            <div class="space-y-2 border-t-[1px] border-b-[1px] md:border-t-0 md:border-b-0 py-[15px] md:py-0">
+                <h5 class="font-bold hidden md:block uppercase">Services</h5>
+                <h5 class="font-bold flex justify-between md:hidden uppercase" onclick="toggleList(this)">Services<span class="plus block md:hidden">+</span></h5>
+
+                <ul class="space-y-2 hidden md:block">
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Resources</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Abhyaas</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Open Test</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Blog</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Classroom Demo</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Campus Ambassador</a></li>
                 </ul>
             </div>
             <div class="space-y-2 border-t-[1px] border-b-[1px] md:border-t-0 md:border-b-0 py-[15px] md:py-0">
-                <h5 class="font-bold hidden md:block">{{__('footer.pages')}}</h5>
-                <h5 class="font-bold flex justify-between md:hidden" onclick="toggleList(this)">{{__('footer.pages')}} <span class="plus block md:hidden">+</span></h5>
+                <h5 class="font-bold hidden md:block uppercase">Company</h5>
+                <h5 class="font-bold flex justify-between md:hidden uppercase" onclick="toggleList(this)">Company <span class="plus block md:hidden">+</span></h5>
 
                 <ul class="space-y-2 hidden md:block">
                     <li><a href="{{ UrlHelper::linkToVision('/about-us') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.about_us')}}</a></li>
-                    <li><a href="{{ UrlHelper::linkToVision('/centers') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.locations')}}</a></li>
-                    <li><a href="{{ UrlHelper::linkToVision('/contact-us') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.contact_us')}}</a></li>
+                    {{-- <li><a href="{{ UrlHelper::linkToVision('/centers') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.locations')}}</a></li> --}}
+                    {{-- <li><a href="{{ UrlHelper::linkToVision('/contact-us') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.contact_us')}}</a></li> --}}
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Our Centers</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Contact Us</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">FAQ</a></li>
+                    <li><a href="javascript:void(0)" class="text-sm hover:text-[#005FAF]">Syllabus</a></li>
+                </ul>
+            </div>
+            <div class="space-y-2 border-t-[1px] md:border-t-0 py-[15px] md:py-0">
+                <h5 class="font-bold hidden md:block uppercase">{{__('footer.policy')}}</h5>
+                <h5 class="font-bold flex justify-between md:hidden uppercase" onclick="toggleList(this)">{{__('footer.policy')}} <span class="plus">+</span></h5>
+
+                <ul class="space-y-2 hidden md:block">
+                    <li><a href="{{ UrlHelper::linkToVision('/terms-of-use') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.terms_of_use')}}</a></li>
+                    <li><a href="{{ UrlHelper::linkToVision('/privacy') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.privacy_policy')}}</a></li>
+                    <li><a href="{{ UrlHelper::linkToVision('/refund-policy') }}" class="text-sm hover:text-[#005FAF]">{{__('footer.refund_policy')}}</a></li>
+                    <li><a href="{{ UrlHelper::linkToVision('/terms-of-use') }}" class="text-sm hover:text-[#005FAF]">Data Policy</a></li>
                 </ul>
             </div>
         </div>
