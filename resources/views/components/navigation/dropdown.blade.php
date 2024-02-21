da398299aebd27a1a8dbee1d6fdb720bdde5915a
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-02-21 16:51:51 +0530

    update link menu

--- resources/views/components/navigation/dropdown.blade.php
+++ resources/views/components/navigation/dropdown.blade.php
@@ -9,17 +9,17 @@
         <div class="border-bottom">
        <x-buttons.primary button-text="{!! $buttonText !!}" button-link="{{ $buttonLink }}"  /> 
        @if($menuData['initiative_id'] === InitiativesHelper::getInitiativeID(Initiatives::MONTHLY_MAGAZINE) && count($menuData['data']) > 0 )
-       <p>{{Carbon::parse($menuData['data'][array_key_first($menuData['data'])][0]->publishedAt)->monthName}}</p>
+       <p class="text-sm flex px-4 pb-[15px] font-medium">{{Carbon::parse($menuData['data'][array_key_first($menuData['data'])][0]->publishedAt)->monthName}}</p>
        @endif
        @if($menuData['initiative_id'] === InitiativesHelper::getInitiativeID(Initiatives::WEEKLY_FOCUS))
-       <p>{{$menuData['data'][array_key_first($menuData['data'])][0]->article[0]->title}}</p>
+       <p class="text-sm flex px-4 pb-[15px] font-medium">{{$menuData['data'][array_key_first($menuData['data'])][0]->article[0]->title}}</p>
        @endif
         </div>
             @foreach ($menuData['data'] as $mainMenu => $subMenu)
                 @if(!empty($subMenu))
                     <li class="relative">
                         <a  href="#"
-                            class="flex items-center justify-between mx-4 py-3 hover:brand-color hover:bgcolor-gray-F4F6FC firstlabelMenu"
+                            class="flex items-center justify-between py-3 hover:brand-color hover:bgcolor-gray-F4F6FC firstlabelMenu"
                             @mouseenter="isMenuOpen = 'menu{{ $menuData['initiative_id'] . $loop->iteration }}'"
                             @click.outside="isMenuOpen = null"
                         >
@@ -29,7 +29,7 @@
                                     : $mainMenu
                                 }}
                             </span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                 <path d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z" fill="#8F93A3"/>
                             </svg>
                         </a>
