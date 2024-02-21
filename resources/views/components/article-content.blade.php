3b87868e343c1240efc94fbec45de5d7c52f1552
Vikram <itsworkingmind@gmail.com>
2024-02-21 12:42:30 +0530

    active div on select in daily news archive

--- resources/views/components/article-content.blade.php
+++ resources/views/components/article-content.blade.php
@@ -93,6 +93,13 @@
     <div id="article-content" onmouseup="handleSelection()" class="mt-4 printable-area">
         {!! $article->content !!}
     </div>
+
+            <ul class="flex justify-start items-baseline mt-4">
+            <li class="text-[#3D3D3D] text-base mr-2">Tags :</li>
+            @foreach ($article->tags as $tag)
+                <li class="mr-2 bg-[#F4F6F8] text-xs rounded-sm py-1 px-2 cursor-pointer">{{ $tag->name }}</li>
+            @endforeach     
+        </ul>
 </div>
 
 <script>
