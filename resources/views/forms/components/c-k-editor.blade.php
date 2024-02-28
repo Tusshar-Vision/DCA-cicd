e602fe0736599aa21571f0a47c0ca903ba76ada2
Yash Tiwari <yash.tiwari@visionias.in>
2024-02-28 13:50:52 +0530

    Ckeditor integration complete with all the features

--- resources/views/forms/components/c-k-editor.blade.php
+++ resources/views/forms/components/c-k-editor.blade.php
@@ -6,10 +6,6 @@
     :field="$field"
 >
     <style>
-        .ck-editor-container {
-            display: flex;
-            width: 100%;
-        }
         .ck-editor__editable {
             min-height: 500px; /* Adjust the height as needed */
         }
@@ -21,9 +17,6 @@
         .ck-editor__editable ol {
             margin-left: 25px;
         }
-        .ck.ck-toolbar.ck-toolbar_grouping>.ck-toolbar__items{
-            flex-wrap: wrap !important;
-        }
     </style>
 
     <div
@@ -33,7 +26,6 @@
         x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('ckeditor'))]"
         ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('ck-editor-component') }}"
         x-data="ckEditorComponent({ state: $wire.{{ $applyStateBindingModifiers("entangle('{$statePath}')", isOptimisticallyLive: false) }} })"
-        class="ck-editor-container"
     >
         <textarea x-ref="editor" hidden></textarea>
     </div>
