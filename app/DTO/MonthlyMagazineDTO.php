f83b13de778925a99dda187ac73d2e4b3b9243af
Yash Tiwari <yash.tiwari@visionias.in>
2024-03-10 02:38:09 +0530

    Monthly magazine pagination bug fixed

--- app/DTO/MonthlyMagazineDTO.php
+++ app/DTO/MonthlyMagazineDTO.php
@@ -11,12 +11,35 @@ class MonthlyMagazineDTO extends PublishedInitiativeDTO
 {
     public array $topics;
     public Collection $sortedArticlesWithTopic;
+    private Collection $sortedArticlesWithoutTopic;
 
     private function sortArticlesWithTopic(): void
     {
         $articlesCollection = collect($this->articles->all())->merge($this->shortArticles->all());
         $this->sortedArticlesWithTopic = $articlesCollection->groupBy('topic');
+        $this->sortedArticlesWithoutTopic = collect();
+
+        $this->sortedArticlesWithTopic->map(function ($topic) {
+           $this->sortedArticlesWithoutTopic = $this->sortedArticlesWithoutTopic->merge($topic->all());
+        });
+
+        $this->articles = ArticleDTO::collection($this->sortedArticlesWithoutTopic);
     }
+
+    public function getArticleIndexFromSlug($slug): int|null
+    {
+        $foundIndex = null;
+
+        $this->sortedArticlesWithoutTopic->each(function ($article, $index) use ($slug, &$foundIndex) {
+            if ($article->slug === $slug) {
+                $foundIndex = $index;
+                return false; // Stop the iteration once the article is found
+            }
+        });
+
+        return $foundIndex;
+    }
+
 
     public static function fromModel(PublishedInitiative $publishedInitiative): MonthlyMagazineDTO
     {
