a91d46a70e6a1625ffc9a5db1dc486f8bf9c10ba
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-03-11 12:40:56 +0530

    text color update

--- resources/views/livewire/widgets/archives/quarterly-revision-document.blade.php
+++ resources/views/livewire/widgets/archives/quarterly-revision-document.blade.php
@@ -6,7 +6,7 @@
                     <path d="M6.86719 15.0156L0.99998 8.49977" stroke="#242424" class="dark:stroke-white" stroke-linecap="round"/>
                     <line x1="0.5" y1="-0.5" x2="9.61301" y2="-0.5" transform="matrix(-0.654931 0.755689 0.654931 0.755689 7.65625 1.30469)" stroke="#242424" class="dark:stroke-white" stroke-linecap="round"/>
                 </svg>
-                <h4 class="text-sm md:text-lg font-normal font-[#242424] ml-2 md:ml-4">Quarterly Revision Documents</h4>
+                <h4 class="text-sm md:text-lg font-normal text-[#242424] dark:text-white ml-2 md:ml-4">Quarterly Revision Documents</h4>
             </div>
             <div class="flex items-center justify-between">
                 <p class="text-sm md:text-base">Filter</p>
