6636feb6a8db2767340ace58ba6937d592bc53a9
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-03-12 17:15:33 +0530

    update menu

--- resources/views/components/navigation/initiatives.blade.php
+++ resources/views/components/navigation/initiatives.blade.php
@@ -306,7 +306,7 @@
 
     <div class="login-wrapper">
         <ul class="flex items-center">
-            <li class="pr-1 lg:pr-4">
+            <li class="pr-1 lg:pr-4 mr-2 lg:mr-0">
                 <a href="javascript:void(0)" onclick="openSearch()">
                     {!! \App\Helpers\SvgIconsHelper::getSvgIcon('search-open') !!}
                 </a>
@@ -325,7 +325,7 @@
                 <li class="pr-[20px] hidden xl:block">
                     <a href="{{ UrlHelper::linkToVision('/register') }}" class="register">Register</a>
                 </li>
-                <li class="pl-[20px]" style="border-left: 1px solid #E5EAF4;">
+                <li class="pl-[10px]" style="border-left: 1px solid #E5EAF4;">
                     <button @click="isAuthFormOpen = !isAuthFormOpen" class="flex items-center text-xs xl:text-sm">
                         <svg class="mr-3 hidden lg:block" width="24" height="24" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
