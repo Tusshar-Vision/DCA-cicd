de7008298dabefe66d1ce9527e8531ec543f8043
SaikatVIAS <saikat.chakraborty@visionias.in>
2024-04-02 16:03:59 +0530

    update editor style

--- resources/views/components/widgets/article-pagination.blade.php
+++ resources/views/components/widgets/article-pagination.blade.php
@@ -1,6 +1,6 @@
 @use('App\Services\ArticleService')
 
-<div class="flex justify-between items-center">
+<div class="flex justify-between items-center mb-10">
     @if($previousArticleIndex !== null)
         @php
             $articleURL = ArticleService::getArticleUrlFromSlug($currentInitiative->articles[$previousArticleIndex]->slug);
@@ -79,8 +79,8 @@
             <a wire:navigate id="nxt-article-btn" href="{{ $articleURL ?? $currentInitiative->articles[$nextArticleIndex]->slug }}">
                 <div class="flex-col text-right md:flex justify-end">
                     <span class="flex justify-end mb-2">
-                        <svg width="24" height="24" viewBox="0 0 24 24" fill="black" class="dark:fill-white" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM12 11H8V13H12V16L16 12L12 8V11Z" />
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="black" class="dark:fill-white mr-2 rotate-180" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 12 22C6.48 22 2 17.52 2 12C2 6.48 6.48 2 12 2ZM12 20C16.42 20 20 16.42 20 12C20 7.58 16.42 4 12 4C7.58 4 4 7.58 4 12C4 16.42 7.58 20 12 20ZM12 11H16V13H12V16L8 12L12 8V11Z" />
                         </svg>
                     </span>
                     <span class="font-semibold">Next Article</span>
