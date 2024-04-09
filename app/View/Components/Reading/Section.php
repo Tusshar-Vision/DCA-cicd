c62e1f96720e93ade9aa12b1bfb88acb1fb1892c
Yash Tiwari <yash.tiwari@visionias.in>
2024-04-09 14:41:59 +0530

    Removed tags from weekly focus, refactored code

--- app/View/Components/Reading/Section.php
+++ app/View/Components/Reading/Section.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace App\View\Components;
+namespace App\View\Components\Reading;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class ArticleContent extends Component
+class Section extends Component
 {
     /**
      * Create a new component instance.
      */
-    public function __construct(public $article)
+    public function __construct(public $articles)
     {
         //
     }
@@ -21,6 +21,6 @@ class ArticleContent extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.article-content');
+        return view('components.reading.section');
     }
 }
--- app/View/Components/Reading/ShortArticle.php
+++ app/View/Components/Reading/ShortArticle.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\View\Components;
+namespace App\View\Components\Reading;
 
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class InshortArticle extends Component
+class ShortArticle extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class InshortArticle extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.inshort-article');
+        return view('components.reading.short-article');
     }
 }
