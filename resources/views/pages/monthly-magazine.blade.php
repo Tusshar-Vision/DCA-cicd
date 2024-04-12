3cfe0ba7f5fb57be105ae0c4c038a83605498364
Yash Tiwari <yash.tiwari@visionias.in>
2024-04-12 16:55:22 +0530

    Fixed responsive table problem

--- resources/views/pages/monthly-magazine.blade.php
+++ resources/views/pages/monthly-magazine.blade.php
@@ -12,11 +12,12 @@
 
 @section('article-content')
     <div x-data="{ openItem: 0, expanded: false }" class="flex flex-col lg:flex-row space-x-0 lg:space-x-8 mt-[20px] md:mt-0">
-        <div class="flex min-w-full lg:min-w-[340px] lg:w-2/6 flex-col space-y-6 leftsticky stickyMl-0">
+        <div class="flex min-w-full max-w-full lg:min-w-[340px] flex-col space-y-6 leftsticky stickyMl-0">
 
             <img src="{{ asset('images/monthly-magazine-logo.svg') }}" alt="Monthly Magazine Logo" />
 
             <livewire:widgets.articles-side-bar :topics="$package->topics" :articles="$package->sortedArticlesWithTopic" :table-of-content="$tableOfContent" />
+
             <div class="hidden lg:block">
                 <x-widgets.side-bar-download-menu initiative="monthly-magazine" :media="$package?->media"/>
             </div>
