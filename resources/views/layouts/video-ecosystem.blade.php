f0f672e740d7e23d3c350ce330eef82a1e47e6cb
Yash Tiwari <yash.tiwari@visionias.in>
2024-05-07 17:15:33 +0530

    Video page menu updated

--- resources/views/layouts/video-ecosystem.blade.php
+++ resources/views/layouts/video-ecosystem.blade.php
@@ -3,36 +3,35 @@
 
 @section('content')
     <x-containers.grid-wide class="mt-6">
-        <div class="flex flex-col md:flex-row justify-between items-center">
-            <h2 class="text-[30px] text-[#040404] mb-2 md:mb-4 dark:text-white w-full md:w-1/4">{{ $title }}</h2>
-            <ul class="flex items-center justify-start space-x-2 overflow-x-auto w-full md:w-3/4 p-2 mb-4">
+        <div class="flex flex-col md:flex-row">
+            <ul class="flex space-x-2 overflow-x-auto p-2 mb-4 md:justify-center flex-grow">
                 <li>
-                    <a href="{{ route('videos') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('videos*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('videos') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('videos*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         Latest Videos
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('daily-news') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] active:text-[#3362CC] hover:text-[#3362CC] {{ request()->is('daily-news*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('daily-news') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] active:text-[#3362CC] hover:text-[#3362CC] {{ request()->is('daily-news*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         News Today
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('in-conversation') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('in-conversation*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('in-conversation') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('in-conversation*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         In Conversation
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('simplified-by-visionias') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('simplified-by-visionias*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('simplified-by-visionias') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('simplified-by-visionias*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         Simplified
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('personality-in-focus') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('personality-in-focus*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('personality-in-focus') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('personality-in-focus*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         Personality in Focus
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('scheme-in-focus') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('scheme-in-focus*') ? 'text-[#3362CC]' : '' }}">
+                    <a href="{{ route('scheme-in-focus') }}" wire:navigate class="text-[14px] text-nowrap rounded-md px-3 py-2 text-[#040404] bg-[#F4F4F6] hover:text-[#3362CC] {{ request()->is('scheme-in-focus*') ? 'text-white !bg-[#3362CC] hover:text-white' : '' }}">
                         Scheme in Focus
                     </a>
                 </li>
