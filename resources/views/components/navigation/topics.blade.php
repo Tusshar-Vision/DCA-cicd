49dd9f5a89204e36069c3376c2c1513c736f7008
Yash Tiwari <yash.tiwari@visionias.in>
2024-05-09 21:48:22 +0530

    Added autoscroll feature to monthly topic bar and video page menu bar

--- resources/views/components/navigation/topics.blade.php
+++ resources/views/components/navigation/topics.blade.php
@@ -3,7 +3,7 @@
     $currentTopic = request()->segment(3);
 @endphp
 
-<ul class="flex h-10 text-white items-center bg-visionBlue space-x-8 whitespace-nowrap overflow-x-auto pr-4">
+<ul id="topic-bar" class="flex h-10 text-white items-center bg-visionBlue space-x-8 whitespace-nowrap overflow-x-auto pr-4">
     <li>
         <p class="text-sm font-bold pl-2">{{ Carbon::parse($publishedDate)->format('F Y') }}</p>
     </li>
@@ -19,7 +19,7 @@
                 $topicName = $topic->first()->topic;
             @endphp
             <a
-                class="mr-4 inline-block {{ ($topicName === $currentTopic) ? 'font-bold' : '' }}"
+                class="mr-4 inline-block {{ ($topicName === $currentTopic) ? 'font-bold active' : '' }}"
                 href="{{ \App\Services\ArticleService::getArticleUrlFromSlug($articleSlug) }}"
                 wire:navigate
             >
@@ -28,3 +28,20 @@
         @endforeach
     </li>
 </ul>
+<script>
+    document.addEventListener('livewire:navigated', () => {
+        let topicBar = document.querySelector('#topic-bar');
+        let element = document.querySelector('.active');
+        console.log(topicBar);
+        console.log(element);
+        if (element) {
+            setTimeout(() => {
+                topicBar.scrollTo({
+                    left: element.offsetLeft - topicBar.offsetLeft,
+                    behavior: 'smooth'
+                });
+            }, 200);
+        }
+    }, {once: true});
+</script>
+
