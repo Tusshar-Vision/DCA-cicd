5daaabe3e9c979d6d225619ee55fc8969642ceb6
Yash Tiwari <yash.tiwari@visionias.in>
2024-05-09 21:54:35 +0530

    Monthly magazine topic bar updates

--- resources/views/components/navigation/topics.blade.php
+++ resources/views/components/navigation/topics.blade.php
@@ -3,37 +3,39 @@
     $currentTopic = request()->segment(3);
 @endphp
 
-<ul id="topic-bar" class="flex h-10 text-white items-center bg-visionBlue space-x-8 whitespace-nowrap overflow-x-auto pr-4">
-    <li>
-        <p class="text-sm font-bold pl-2">{{ Carbon::parse($publishedDate)->format('F Y') }}</p>
-    </li>
-    <li>
-        <svg class="bg-white" width="15" height="2" viewBox="0 0 15 2" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 1H15"/>
-        </svg>
-    </li>
-    <li>
-        @foreach ($topics as $topic)
-            @php
-                $articleSlug = $topic->first()->slug;
-                $topicName = $topic->first()->topic;
-            @endphp
-            <a
-                class="mr-4 inline-block {{ ($topicName === $currentTopic) ? 'font-bold active' : '' }}"
-                href="{{ \App\Services\ArticleService::getArticleUrlFromSlug($articleSlug) }}"
-                wire:navigate
-            >
-                {{ $formatString($topicName) }}
-            </a>
-        @endforeach
-    </li>
-</ul>
+<div class="flex items-center">
+    <ul class="flex h-10 text-white items-center bg-visionBlue space-x-4 whitespace-nowrap pr-4">
+        <li>
+            <p class="text-sm font-bold pl-2">{{ Carbon::parse($publishedDate)->format('F Y') }}</p>
+        </li>
+        <li>
+            <svg class="bg-white" width="15" height="2" viewBox="0 0 15 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 1H15"/>
+            </svg>
+        </li>
+    </ul>
+    <ul id="topic-bar" class="flex h-10 text-white items-center bg-visionBlue space-x-8 whitespace-nowrap overflow-x-auto pr-4">
+        <li>
+            @foreach ($topics as $topic)
+                @php
+                    $articleSlug = $topic->first()->slug;
+                    $topicName = $topic->first()->topic;
+                @endphp
+                <a
+                    class="mr-4 inline-block {{ ($topicName === $currentTopic) ? 'font-bold active' : '' }}"
+                    href="{{ \App\Services\ArticleService::getArticleUrlFromSlug($articleSlug) }}"
+                    wire:navigate
+                >
+                    {{ $formatString($topicName) }}
+                </a>
+            @endforeach
+        </li>
+    </ul>
+</div>
 <script>
     document.addEventListener('livewire:navigated', () => {
         let topicBar = document.querySelector('#topic-bar');
         let element = document.querySelector('.active');
-        console.log(topicBar);
-        console.log(element);
         if (element) {
             setTimeout(() => {
                 topicBar.scrollTo({
